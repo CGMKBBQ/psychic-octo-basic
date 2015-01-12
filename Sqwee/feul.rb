f15daa13ec896f1c5edd63f937d139099ce65827
rockkat <rockkat00@gmail.com>
2015-01-11 17:04:13 -0800

    prettied the comments

--- Sqwee/feul.rb
+++ Sqwee/feul.rb
@@ -1,7 +1,8 @@
-# the sales price of Costco feul 1/11/15
+# written in the car while i waited for Mike to get his hair cut on 1/11/2015
+# the sales price of Costco feul
 price = 1.99
-#ask the capacity of their feul tank
+# ask the capacity of their feul tank
 puts "How many gallons  is your cars feul tank?"
 tank = gets.chomp
-#calculate how much a fill up would cost
+# calculate how much a fill up would cost
 puts "Then to fill your car it will cost #{tank.to_i * price} dollars."
