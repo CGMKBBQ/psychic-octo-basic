2680f2636e0a7c667864279b16e6bca88392ff06
rockkat <rockkat00@gmail.com>
2015-01-11 17:11:34 -0800

    round 2

--- Sqwee/feul.rb
+++ Sqwee/feul.rb
@@ -1,3 +1,4 @@
+begin
 # written in the car while i waited for Mike to get his hair cut on 1/11/2015
 # the sales price of Costco feul
 price = 1.99
@@ -6,3 +7,17 @@ puts "How many gallons  is your cars feul tank?"
 tank = gets.chomp
 # calculate how much a fill up would cost
 puts "Then to fill your car it will cost #{tank.to_i * price} dollars."
+end
+
+begin
+puts "ROUND TWO - GO!"
+# REV 2 - making it dynamic
+# ask sales price
+puts "What is the sales price of today's feul?"
+sales_price = gets.chomp
+# ask gallons
+puts "How many gallons are you purchasing today?"
+purch_gallons = gets.chomp
+#calculate total purchase
+puts "It will cost $ #{sales_price.to_i * purch_gallons.to_i}"
+end
