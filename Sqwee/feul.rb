ae79e16ce2d47b40821b471857d9f3a6b0661156
rockkat <rockkat00@gmail.com>
2015-02-09 19:14:47 -0800

    updated feul

--- Sqwee/feul.rb
+++ Sqwee/feul.rb
@@ -3,7 +3,7 @@ begin
 # the sales price of Costco feul
 price = 1.99
 # ask the capacity of their feul tank
-puts "How many gallons  is your cars feul tank?"
+puts "How many gallons is your cars feul tank?"
 tank = gets.chomp
 # calculate how much a fill up would cost
 puts "Then to fill your car it will cost $#{tank.to_i * price} dollars.\n\n"
@@ -12,6 +12,13 @@ end
 begin
 puts "ROUND TWO - GO!\n\n"
 # REV 2 - making it dynamic
+# ask odometer
+puts "What is the current odometer reading?"
+odometer = gets.chomp.to_i
+# ask trip peter
+puts "What is your current trip meter reading?"
+trip_meter = gets.chomp.to_i
+
 # ask sales price
 puts "What is the sales price of today's feul?"
 sales_price = gets.chomp.to_i
@@ -19,7 +26,9 @@ sales_price = gets.chomp.to_i
 puts "How many gallons are you purchasing today?"
 purch_gallons = gets.chomp.to_i
 # calculate total purchase
+# calulate MPG
 puts "It cost $#{sales_price * purch_gallons}.\n\n"
+puts "Your vehicle got #{trip_meter / purch_gallons} miles per gallon this trip."
 end
 
 <<-PLANS
