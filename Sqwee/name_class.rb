23fb2acd98cecc910c8a5f9a78720fe3c1082d27
rockkat <rockkat00@gmail.com>
2015-03-08 13:36:10 -0700

    class basic excersize

---
+++ Sqwee/name_class.rb
@@ -0,0 +1,23 @@
+class Name
+  def title
+    "Mrs."
+  end
+  
+  def first_name
+    "Sara"
+  end
+  
+  def middle_name
+    "E."
+  end
+  
+  def last_name
+    "Kostur"
+  end
+end
+
+name = Name.new
+puts name.title
+puts name.first_name
+puts name.middle_name
+puts name.last_name
